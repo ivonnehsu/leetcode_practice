4ba7a970528a73d8c440d483ed15bf2ab813bd9a
ivonnehsu <ivonnehsu1@gmail.com>
2024-02-17 17:09:15 -0500

    update day49 021724

--- day49_dp_10.py
+++ day49_dp_10.py
@@ -57,6 +57,6 @@ class Solution(object):
         dp[0][0]=-prices[0] # 0， 持有
         dp[0][1]=0 # 1，不持有
         for i in range(1,len(prices)):
-            dp[i][0]=max(dp[i-1][0],dp[i-1][1]-prices[i])
+            dp[i][0]=max(dp[i-1] [0],dp[i-1][1]-prices[i])
             dp[i][1]=max(dp[i-1][1],dp[i-1][0]+prices[i])
         return dp[-1][1]
